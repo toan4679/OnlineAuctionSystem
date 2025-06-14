2ca4681865ff625b2e1cfb82728eefdc0e6fc0dc
toan4679 <100296354+toan4679@users.noreply.github.com>
2025-06-14 11:53:50 +0700

    Create DFD-Design.m

---
+++ DFD-Design.m
@@ -0,0 +1,8 @@
+# Data Flow Diagram (DFD)
+
+## Context Diagram (DFD Level 0)
+
+```mermaid
+graph TD
+    User[User (Buyer/Seller)] <--> System[Online Auction System]
+    Admin[Administrator] <--> System
